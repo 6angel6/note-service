ebc9bc45423ffee1d9a8a7319cca669680d96dcf
r_angel <ruslanshaman001@gmail.com>
2025-01-25 16:19:48 +0500

    d

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -9,14 +9,14 @@ import (
 
 func (h *Handler) login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-
+ 
 	var user dto.UserRequest
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid dto payload"})
 		return
 	}
-
+    //
 	tokens, err := h.service.Login(user.Username, user.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
